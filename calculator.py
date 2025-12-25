8d92333e02fc67c10f1ed20cd35fe78de96e7ee6
Kyaw Min Khant @ Jasper <minkhantk317@gmail.com>
2025-12-25 05:04:33 +0000

    Refactor calculator layout and add memory functions
    Updated the calculator's code structure and added a memory frame for memory-related keys.

--- calculator.py
+++ calculator.py
@@ -23,14 +23,19 @@ Author: Kyaw Min Khant
 
 from tkinter import *
 
+# Initialising
 root=Tk()
 root.title("Simple Calculator")
 
-d=Entry(root, width=20, borderwidth=5, font=("Adobe-Garamond-Pro", 12)) # To show previous calculations
-d.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
+# Setting up small fram at row 2, column 0 for memory related keys
+memoryFrame=Frame(root)
+memoryFrame.grid(row=2, column=0, columnspan=3, sticky="w", padx=5, pady=2)
 
-e=Entry(root, width=35, borderwidth=5, font="Adobe-Garamond-Pro") # Inputs
-e.grid(row=1, column=0, columnspan=5, padx=10, pady=10)
+d=Entry(root, width=20, borderwidth=5, font=("Adobe-Garamond-Pro", 12)) # To show previous calculations
+d.grid(row=0, column=0, columnspan=5, padx=8, pady=(8, 2), sticky="w")
+
+e=Entry(root, width=30, borderwidth=5, font="Adobe-Garamond-Pro") # Inputs
+e.grid(row=1, column=0, columnspan=5, padx=8, sticky="w")
 
 math="" # Undefined math variable
 resultShown=False # No result on screen
@@ -66,7 +71,7 @@ def roundFind(number):
 
 # Record data into top text box to allow user to see the calculations
 def record(element):
-    if element=="Zerror":
+    if element=="Zerror": # Zero Error Case, shut all other key except AC
         d.delete(0, END)
         button_0.config(state=DISABLED)
         button_1.config(state=DISABLED)
@@ -96,7 +101,7 @@ def record(element):
         button_mP.config(state=DISABLED)
         button_mN.config(state=DISABLED)
         button_ms.config(state=DISABLED)
-    else:
+    else: # If not record it as usual
         current=d.get()
         d.delete(0, END)
         d.insert(0, str(current)+roundFind(element))
@@ -107,7 +112,7 @@ def equal():
     result=None
     
     try:
-        if math!="" and e.get()!="":
+        if math!="" and e.get()!="": # If there is an arithmetic operator assigned
             second_number=e.get()
             e.delete(0, END)
             if math=="+":
@@ -126,16 +131,25 @@ def equal():
             num1Exist=False
             return result
         
-        elif math=="" and e.get()!="":
+        elif math=="" and e.get()!="": # If there is no operations but just a number
             current=e.get()
             d.delete(0, END)
             e.delete(0, END)
-            e.insert(0, current)
+            if any(op in current for op in ["+", "-", "*", "/", "^", "√"]):
+                if "^" in current:
+                    current2=current.replace("^", "**") # Replace ^ with ** so it can function properly
+                elif "√" in current:
+                    currentA=current.replace("√", "") # Temporary store 
+                    current2=f"({currentA})**0.5"
+                result=str(roundFind(eval(current2))) # Evaluate the equation normally if there is no power
+                e.insert(0, result)
+            else:
+                e.insert(0, current)   
             record(current)
             record("=")
             resultShown=True
             
-    except ZeroDivisionError:
+    except ZeroDivisionError: # Zero Error case
         e.delete(0, END)
         result="Zero Division Error!"
         e.insert(0, result)
@@ -149,20 +163,20 @@ def addition():
     global num1Exist, resultShown
     
     resultShown=False
-    if num1Exist==False:
-        if e.get()!="":
+    if num1Exist==False: # If there is no first number
+        if e.get()!="": # If there is an input
             first_num=e.get()
             global f_num
             global math
-            math="+"
-            f_num=float(first_num)
-            num1Exist=True
-            d.delete(0, END)
-            e.delete(0, END)
-            record(f_num)
-            record(math)
-    else:
-        f_num=equal()
+            math="+" # Assign the Arthmetic Operator
+            f_num=float(first_num) # Number can be float
+            num1Exist=True # First number now exist
+            d.delete(0, END) # Clear previous record
+            e.delete(0, END) # Clear input
+            record(f_num) # Record the first number
+            record(math) # Record the operator
+    else: # If there is first number
+        f_num=equal() # New first number will be the result of the calculation for further calculation
         d.delete(0, END)
         e.delete(0, END)
         math="+"
@@ -245,7 +259,7 @@ def division():
     
 # x related
 def reciprocal():
-    try:
+    try: 
         if e.get()!="":
             current=float(e.get())
             e.delete(0, END)
@@ -387,82 +401,81 @@ def MS():
     
     
 #Buttons
-button_1=Button(root, text="1", padx=40, pady=20, command=lambda: click(1), borderwidth=2)
-button_2=Button(root, text="2", padx=40, pady=20, command=lambda: click(2), borderwidth=2)
-button_3=Button(root, text="3", padx=40, pady=20, command=lambda: click(3), borderwidth=2)
-button_4=Button(root, text="4", padx=40, pady=20, command=lambda: click(4), borderwidth=2)
-button_5=Button(root, text="5", padx=40, pady=20, command=lambda: click(5), borderwidth=2)
-button_6=Button(root, text="6", padx=40, pady=20, command=lambda: click(6), borderwidth=2)
-button_7=Button(root, text="7", padx=40, pady=20, command=lambda: click(7), borderwidth=2)
-button_8=Button(root, text="8", padx=40, pady=20, command=lambda: click(8), borderwidth=2)
-button_9=Button(root, text="9", padx=40, pady=20, command=lambda: click(9), borderwidth=2)
-button_0=Button(root, text="0", padx=40, pady=20, command=lambda: click(0), borderwidth=2)
+button_1=Button(root, text="1", width=12, height=4, command=lambda: click(1), borderwidth=2)
+button_2=Button(root, text="2", width=12, height=4, command=lambda: click(2), borderwidth=2)
+button_3=Button(root, text="3", width=12, height=4, command=lambda: click(3), borderwidth=2)
+button_4=Button(root, text="4", width=12, height=4, command=lambda: click(4), borderwidth=2)
+button_5=Button(root, text="5", width=12, height=4, command=lambda: click(5), borderwidth=2)
+button_6=Button(root, text="6", width=12, height=4, command=lambda: click(6), borderwidth=2)
+button_7=Button(root, text="7", width=12, height=4, command=lambda: click(7), borderwidth=2)
+button_8=Button(root, text="8", width=12, height=4, command=lambda: click(8), borderwidth=2)
+button_9=Button(root, text="9", width=12, height=4, command=lambda: click(9), borderwidth=2)
+button_0=Button(root, text="0", width=12, height=4, command=lambda: click(0), borderwidth=2)
 
-button_negate=Button(root, text="+/-", padx=34, pady=20, command=negate, borderwidth=2)
-button_decimal=Button(root, text=".", padx=41, pady=20, command=decimal, borderwidth=2)
+button_negate=Button(root, text="+/-", width=12, height=4, command=negate, borderwidth=2)
+button_decimal=Button(root, text=".", width=12, height=4, command=decimal, borderwidth=2)
 
-button_add=Button(root, text="+", padx=39, pady=20, command=addition, borderwidth=2)
-button_minus=Button(root, text="-", padx=40, pady=20, command=subtraction, borderwidth=2)
-button_multiply=Button(root, text="*", padx=40, pady=20, command=multiplication, borderwidth=2)
-button_divide=Button(root, text="/", padx=40, pady=20, command=division, borderwidth=2)
+button_add=Button(root, text="+", width=12, height=4, command=addition, borderwidth=2)
+button_minus=Button(root, text="-", width=12, height=4, command=subtraction, borderwidth=2)
+button_multiply=Button(root, text="*", width=12, height=4, command=multiplication, borderwidth=2)
+button_divide=Button(root, text="/", width=12, height=4, command=division, borderwidth=2)
 
-button_reciprocal=Button(root, text="⅟x", padx=37, pady=20, command=reciprocal, borderwidth=2)
-button_square=Button(root, text="x²", padx=38, pady=20, command=square, borderwidth=2)
-button_root=Button(root, text="√x", padx=36, pady=20, command=square_root, borderwidth=2)
+button_reciprocal=Button(root, text="⅟x", width=12, height=4, command=reciprocal, borderwidth=2)
+button_square=Button(root, text="x²", width=12, height=4, command=square, borderwidth=2)
+button_root=Button(root, text="√x", width=12, height=4, command=square_root, borderwidth=2)
 
-button_percentage=Button(root, text="%", padx=38, pady=20, command=percent, borderwidth=2)
+button_percentage=Button(root, text="%", width=12, height=4, command=percent, borderwidth=2)
 
-button_clear_entry=Button(root, text="CE", padx=36, pady=20, command=ce, borderwidth=2)
-button_clear=Button(root, text="AC", padx=35, pady=20, command=ac, borderwidth=2)
-button_backspace=Button(root, text="<⊏", padx=33, pady=20, command=backspace, borderwidth=2)
-button_equal=Button(root, text="=", padx=39, pady=20, command=equal, borderwidth=2)
+button_clear_entry=Button(root, text="CE", width=12, height=4, command=ce, borderwidth=2)
+button_clear=Button(root, text="AC", width=12, height=4, command=ac, borderwidth=2)
+button_backspace=Button(root, text="<⊏", width=12, height=4, command=backspace, borderwidth=2)
+button_equal=Button(root, text="=", width=12, height=4, command=equal, borderwidth=2)
 
-button_mC=Button(root, text="MC", padx=34, pady=20, command=MC, borderwidth=2, state=DISABLED)
-button_mR=Button(root, text="MR", padx=34, pady=20, command=MR, borderwidth=2, state=DISABLED)
-button_mP=Button(root, text="M+", padx=34, pady=20, command=MP, borderwidth=2)
-button_mN=Button(root, text="M-", padx=34, pady=20, command=MN, borderwidth=2)
-button_ms=Button(root, text="MS", padx=34, pady=20, command=MS, borderwidth=2)
+button_mC=Button(memoryFrame, text="MC", font=("Adobe-Garamond-Pro", 8), width=7, height=2, command=MC, borderwidth=2, state=DISABLED)
+button_mR=Button(memoryFrame, text="MR", font=("Adobe-Garamond-Pro", 8), width=7, height=2, command=MR, borderwidth=2, state=DISABLED)
+button_mP=Button(memoryFrame, text="M+", font=("Adobe-Garamond-Pro", 8), width=7, height=2, command=MP, borderwidth=2)
+button_mN=Button(memoryFrame, text="M-", font=("Adobe-Garamond-Pro", 8), width=7, height=2, command=MN, borderwidth=2)
+button_ms=Button(memoryFrame, text="MS", font=("Adobe-Garamond-Pro", 8), width=7, height=2, command=MS, borderwidth=2)
 
 
 
 #Buttons Placement
-button_mC.grid(row=2, column=0)
-button_mR.grid(row=2, column=1)
-button_mP.grid(row=2, column=2)
-button_mN.grid(row=2, column=3)
-button_ms.grid(row=2, column=4)
+button_mC.grid(row=0, column=0, padx=1)
+button_mR.grid(row=0, column=1, padx=1)
+button_mP.grid(row=0, column=2, padx=1)
+button_mN.grid(row=0, column=3, padx=1)
+button_ms.grid(row=0, column=4, padx=1)
 
-button_percentage.grid(row=3, column=0)
+button_percentage.grid(row=3, column=0, padx=(5, 1))
 button_clear_entry.grid(row=3, column=1)
 button_clear.grid(row=3, column=2)
-button_backspace.grid(row=3, column=3)
+button_backspace.grid(row=3, column=3, padx=(1, 5))
 
-button_reciprocal.grid(row=4, column=0)
-button_square.grid(row=4, column=1)
+button_reciprocal.grid(row=4, column=0, padx=(5, 1))
+button_square.grid(row=4, column=1, padx=(0, 1))
 button_root.grid(row=4, column=2)
-button_divide.grid(row=4, column=3)
+button_divide.grid(row=4, column=3, padx=(1, 5))
 
-button_7.grid(row=5, column=0)
-button_8.grid(row=5, column=1)
+button_7.grid(row=5, column=0, padx=(5, 1))
+button_8.grid(row=5, column=1, padx=(0, 1))
 button_9.grid(row=5, column=2)
-button_multiply.grid(row=5, column=3)
+button_multiply.grid(row=5, column=3, padx=(1, 5))
 
-button_4.grid(row=6, column=0)
-button_5.grid(row=6, column=1)
+button_4.grid(row=6, column=0, padx=(5, 1))
+button_5.grid(row=6, column=1, padx=(0, 1))
 button_6.grid(row=6, column=2)
-button_minus.grid(row=6, column=3)
+button_minus.grid(row=6, column=3, padx=(1, 5))
 
-button_1.grid(row=7, column=0)
-button_2.grid(row=7, column=1)
+button_1.grid(row=7, column=0, padx=(5, 1))
+button_2.grid(row=7, column=1, padx=(0, 1))
 button_3.grid(row=7, column=2)
-button_add.grid(row=7, column=3)
+button_add.grid(row=7, column=3, padx=(1, 5))
 
-button_negate.grid(row=8, column=0)
-button_0.grid(row=8, column=1)
-button_decimal.grid(row=8, column=2)
+button_negate.grid(row=8, column=0, padx=(5, 1), pady=(0, 5))
+button_0.grid(row=8, column=1, padx=(0, 1), pady=(0, 5))
+button_decimal.grid(row=8, column=2, pady=(0, 5))
 
-button_equal.grid(row=8, column=3)
+button_equal.grid(row=8, column=3, padx=(1, 5), pady=(0, 5))
 
 
 root.mainloop()
-
